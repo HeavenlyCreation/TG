faebd39339f589471640ad29b5d8e732f3c70c54
jbo2014 <jbo2014haha@outlook.com>
2016-06-27 09:45:44 +0800

    Revert "Nothing"
    This reverts commit bada3f3bd1916431047a3168e36c294c001200d9.

--- public/index.php
+++ public/index.php
@@ -3,12 +3,5 @@ require __DIR__.'/../vendor/autoload.php';
 
 echo "Hello World!";
 
-// $a = new Tg\Helper\Functions();
-// echo "<br/>".$a->test();
-
-use Luracast\Restler\Restler;
-$r = new Restler();
-$r->addAPIClass('//Man//Controllers//TestController');
-$r->handle();
-// $test = new Man\Controllers\TestController();
-// $test->getAA();
+$a = new Tg\Helper\Functions();
+echo "<br/>".$a->test();
