bada3f3bd1916431047a3168e36c294c001200d9
jbo2014 <jbo2014haha@outlook.com>
2016-06-27 09:32:38 +0800

    Nothing

--- public/index.php
+++ public/index.php
@@ -3,5 +3,12 @@ require __DIR__.'/../vendor/autoload.php';
 
 echo "Hello World!";
 
-$a = new Tg\Helper\Functions();
-echo "<br/>".$a->test();
+// $a = new Tg\Helper\Functions();
+// echo "<br/>".$a->test();
+
+use Luracast\Restler\Restler;
+$r = new Restler();
+$r->addAPIClass('//Man//Controllers//TestController');
+$r->handle();
+// $test = new Man\Controllers\TestController();
+// $test->getAA();
